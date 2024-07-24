f56adc86dbfa43f907d4e5fd0053dcb6d4cf8a3e
NuttyYster <72044460+NuttyYster@users.noreply.github.com>
2024-07-24 15:43:41 +0200

    Add files via upload

---
+++ assessment.grad001.nuttymokgapa/assessment.grad001.nuttymokgapa/src/test/java/com/eviro365/assessment/grad001/nuttymokgapa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eviro365.assessment.grad001.nuttymokgapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
