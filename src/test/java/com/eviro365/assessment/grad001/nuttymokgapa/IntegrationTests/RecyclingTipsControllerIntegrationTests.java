fad105822f73a7a25f2f7fd30e97a0f4845b029b
Nutty Mokgapa <nuttylesibana@gmail.com>
2024-08-19 21:40:54 +0200

    First Integration test added

--- src/test/java/com/eviro365/assessment/grad001/nuttymokgapa/IntegrationTests/RecyclingTipsControllerIntegrationTests.java
+++ src/test/java/com/eviro365/assessment/grad001/nuttymokgapa/IntegrationTests/RecyclingTipsControllerIntegrationTests.java
@@ -1,5 +1,6 @@
 package com.eviro365.assessment.grad001.nuttymokgapa.IntegrationTests;
 
+import com.eviro365.assessment.grad001.nuttymokgapa.model.RecyclingTips;
 import com.eviro365.assessment.grad001.nuttymokgapa.repository.RecyclingTipsRepository;
 import com.eviro365.assessment.grad001.nuttymokgapa.service.RecyclingTipsService;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,4 +21,32 @@ import static org.hamcrest.Matchers.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RecyclingTipsControllerIntegrationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private RecyclingTipsRepository repository;
+
+    @Autowired
+    private RecyclingTipsService service;
+
+    @BeforeEach
+    public void setup() {
+        repository.deleteAll();
+        repository.save(new RecyclingTips(1L, "peel","remove all the extra coverings"));
+        repository.save(new RecyclingTips(2L, "wipe", "clean the glass properly"));
+    }
+
+    @Test
+    public void getAllTipsTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/recycling-tips")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", is("peel")))
+                .andExpect(jsonPath("$[1].name", is("wipe")));
+    }
+
+
 }
